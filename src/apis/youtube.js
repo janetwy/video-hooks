c8755f898db7765eece5c1bb02a50c6981df9e39
Janet Tang <tang.j56@gmail.com>
2022-10-25 14:26:50 -0500

    remove api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAi-5un4mToZghtDn4-wwlrB_jyMBgHPS0";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
